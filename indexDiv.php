f7530748c8136e53c926abf92184d2410aa340ff
Thiago Vieira <thiagovieiraas@gmail.com>
2015-02-08 12:42:35 -0600

    I separated the index in various files

---
+++ indexDiv.php
@@ -0,0 +1,24 @@
+<div class="" id="mainBox">
+    <div id="aboutBox" class="container">
+
+        <div class="text1">
+            <h1>Hello Adventurous Traveler</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua maeuh maseuh maseihjn ae kanwu iasyd waud a</p> 
+
+        </div>
+        <div class="space1">
+            <img src="ico1.png" width="120" height="70"><br>
+            Tell us your budget
+        </div>
+        <div class="space1">
+            <img src="ico2.png" width="70" height="70"><br>
+            Pick a date
+        </div>
+        <div class="space1">
+            <img src="ico3.png" width="70" height="70"><br>
+            Tell us what you like
+        </div>
+    </div>
+</div>
+<div class="button" id="bStart"><a href="#"> Get Started!</a></div>
