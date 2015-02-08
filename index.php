c5332f8ba44e97a5bb54dc7091d0a6a293cae490
cottalucas <lucas.oliveira.cotta@gmail.com>
2015-02-08 11:19:34 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<title>Mistery Trip</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	</head>
+	<body>
+		<header id="menuTop">
+			<div class="container">
+				<ul class="nav nav-justified">
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+					<li>
+						<a href="#">Blog</a>
+					</li>
+					<li>
+						<a href="#">Social Medias</a>
+					</li>
+				</ul>
+			</div>
+		</header>
+		<section>
+			<div class="" id="mainBox">
+				<div id="aboutBox" class="container">
+
+					<div class="text1">
+						<h1>Hello Adventurous Traveler</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua.</p> 
+
+					</div>
+					<div class="space1">
+						<img src="ico1.png" width="120" height="70"><br>
+						Tell us your budget
+					</div>
+					<div class="space1">
+						<img src="ico2.png" width="70" height="70"><br>
+						Pick a date
+					</div>
+					<div class="space1">
+						<img src="ico3.png" width="70" height="70"><br>
+						Tell us what you like
+					</div>
+				</div>
+			</div>
+			<div class="button" id="bStart"><a href="#"> Get Started!</a></div>
+		</section>
+			<footer></footer>
+			
+	</body>
+</html>
---
+++ styleJ.js
@@ -0,0 +1,6 @@
+
+
+function setBgSize(){
+
+	document.getElementById("aboutBox").style.backgroundSize = document.width + "," + (document.height/2);
+}
