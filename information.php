229f0a8143f790c6f0d573a07b2903403771bb33
Thiago Vieira <thiagovieiraas@gmail.com>
2015-02-08 16:22:10 -0600

    Fixed some typos and I'm trying to fix the error of the facebook button

--- information.php
+++ information.php
@@ -8,7 +8,8 @@
             
             <!-- FACEBOOK LOGIN BUTTON -->
             <div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="true" data-auto-logout-link="false"></div>
+        </div>
     </div>
+    <div class="button" id="bStart"><a href="#">Back!</a></div> <br />
+    <div class="button" id="bStart"><a href="#">Next!</a></div>
 </div>
-<div class="button" id="bStart"><a href="#">Back!</a></div> <br />
-<div class="button" id="bStart"><a href="#">Next!</a></div>
